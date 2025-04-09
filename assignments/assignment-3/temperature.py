1d0e104306e7db6eb47c847c487d36496e6ef4dc
sara isabella barajas-cruz <saraisabella@saras-MacBook-Air-2.local>
2025-04-08 22:11:57 -0400

    Part 1 changed

--- assignments/assignment-3/temperature.py
+++ assignments/assignment-3/temperature.py
@@ -1,11 +1,9 @@
 #2443227
 #Sara ISabella Barajas-Cruz
 
-# toCelsius
-def toCelsius():
-    f = int(input('Temperature in fahrenheit:'))
-    c = (f - 32)*(5/9)
-    return round( c, 1)
+# question 1
+def toCelsius(f):
+    return round( (f - 32) * (5/9), 2)
 
 '''
 Download the file data.txt from Moodle. In your main
@@ -18,23 +16,21 @@ HINT: the map function may be useful to do fast change of floats from strings an
 Fahrenheit to Celsius.
 3.
 '''
-temp_dict = {}
+# question 2
 input_file = open('data.txt', 'r')
 
+temp_dict = {}
+for line in input_file:
+    lines = line.split()
 
-# for lines in input_file:
-#     lines = lines.readlines().split()
-#     print(lines)
-#     if lines[0][0:3] == '19':
+    if lines and lines[0].isdigit():
+        year = int(lines[0])
 
-#         year = lines[0]
-#         temp_lst = lines[1:]
+        if 1964 <= year <= 1975:
+            fahrenheit_temp = list(map(float, lines[1:]))
+            celsius_temp = list(map(toCelsius, fahrenheit_temp))
+            temp_dict[year] = celsius_temp
 
-#         for i in temp_lst:
-#             list = map(toCelsius(), temp_lst)
-#             temp_dict[year] = list
-# print(temp_dict)
-# DOESNT RUN
 input_file.close()
 '''
 (time estimate: 10 minutes) Write a function called avgTempYear that takes two arguments: a
@@ -44,48 +40,59 @@ provided year is not in the dictionary, your code should use exceptions to handl
 done in class by using the try-except-else block as needed. In the case of invalid year,
 you should print a friendly message and your function in such a case should return nothing.
 '''
-    
-# def avgTempYear(d, year):
-#     try:
-#         for keys in d: 
-#             d.get(year)
-#     except None:
-#         print('This year isn\'t in the dictionnary')
-#     else:
-#         average = sum(d[year]) / len(d[year])
-#         return round(average, 1)
+# question 3 
+def avgTempYear(d, year):
+    try:
+        average = sum(d[year]) / len(d[year])
+    except KeyError :
+        print('This year isn\'t in the dictionnary')
+    else:  
+        return round(average, 2)
+
 '''(time estimate: 10 minutes) Write a function topThreeYears that takes a dictionary (of
 same format as you read from the data.txt file) and returns a list of the three largest averages
 in descending order among all the years. Pay attention to what data structures you may use for
 efficiency.
+
+'''
+# question 4
+
+
+def topThreeYears(d):
+    top_3 = [-1000,-1000,-1000]
+    for year in d:
+        average_temp = avgTempYear(d, year)    
+        if average_temp > top_3[0]:
+            top_3[0] = top_3[1]
+            top_3[1] = top_3[2]
+            top_3[0] = average_temp
+
+        elif average_temp > top_3[1]:
+            top_3[1] = top_3[0]
+            top_3[1] = average_temp
+
+        elif average_temp > top_3[2]:
+            top_3[2] = average_temp
+
+    return top_3
+
+    
+'''
 5. (time estimate: 15 minutes) Write a function called avgTempMonth that takes a dictionary (of
 same format as you read from the data.txt file) and a month (as a string of 3 characters) and
 returns the average temperature for the given month across all years rounded to 2 decimal places.
 Note that you may create a dictionary in your function to help you go back and forth between
 string and integer representation of months.
-For example: month_dict = {‘JAN’: 1, ‘FEB’: 2, ‘MAR’: 3, etc.}
+
 '''
-# qst 4
-# def topThreeYears(d, lst):
-#     list = []
-#     for key in d:
-#         list.append(avgTempYear(d, year))
-#     for i in list:
-#         if list[i] < list[i+1]:
-#             list[i], list[i+1] = list[i+1], list[i]
-#     lst = list[:4]
-#     return lst
-#DOESNT RUN
+# question 5
+def avgTempMonth(d, month):
+    month = month.upper()
+    month_dict = {'JAN': 0, 'FEB': 1, 'MAR': 2, 'APR': 3, 'MAY': 4, 'JUN': 5, 'JUL': 6, 'AUG': 7, 'SEP': 8, 'OCT': 9, 'NOV': 10, 'DEC': 11}
+    month_index = month_dict[month]
     
-'''
-# # qst 5
-# def avgTempMonth(d, month):
-#     month_dict = {}
-#     months = ['JAN', 'FEB',	'MAR',	'APR',	'MAY',	'JUN',	'JUL',	'AUG',	'SEP',	'OCT',	'NOV',	'DEC']
-#     for year in d:
-
-
-#     d[month] = 
-
-#DOESNT RUN
-'''
+    total = 0
+    n_years = len(d)
+    for year in d:
+        total += d[year][month_index]
+    return round(total / n_years, 2)
